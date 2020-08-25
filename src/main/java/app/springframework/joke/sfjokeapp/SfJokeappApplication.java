b3014be67cdc34889b1ced6a7a04c4392b4be810
NishantKatoch <nkatoch3@gmail.com>
2020-08-25 15:15:00 +0530

    XML Based Confriguartion. closed #2

--- src/main/java/app/springframework/joke/sfjokeapp/SfJokeappApplication.java
+++ src/main/java/app/springframework/joke/sfjokeapp/SfJokeappApplication.java
@@ -2,8 +2,10 @@ package app.springframework.joke.sfjokeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class SfJokeappApplication {
 
 	public static void main(String[] args) {
